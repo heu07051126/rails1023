9795cbc91bf79c29fc46cd7373cdc95389058a90
wangzhuoyang <heu07051126@gmail.com>
2017-04-22 13:40:33 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,12 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   def index
     @groups = Group.all
   end
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page =>5) 
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page =>5)
   end
 
   def edit
@@ -46,6 +46,34 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, flash[:alert] = "Group deleted"
   end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论组成功！"
+    else
+      flash[:wraning] = "您已经是本讨论组成员"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已经退出本讨论组~"
+    else
+      flash[:warning] = "你不是本讨论组成员~"
+    end
+
+    redirect_to group_path(@group)
+
+  end
+
 
   private
 
